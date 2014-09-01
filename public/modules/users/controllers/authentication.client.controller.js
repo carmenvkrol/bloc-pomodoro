975f3a909224d95d0607a5cafef63f6a7dc468f7
Carmen V. Krol <carmen.krol@gmail.com>
2014-09-01 08:18:12 -0400

    dashboard page

--- public/modules/users/controllers/authentication.client.controller.js
+++ public/modules/users/controllers/authentication.client.controller.js
@@ -12,8 +12,8 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$http
 				// If successful we assign the response to the global user model
 				$scope.authentication.user = response;
 
-				// And redirect to the index page
-				$location.path('/');
+				// And redirect to the dashboard
+				$location.path('/dashboard');
 			}).error(function(response) {
 				$scope.error = response.message;
 			});
@@ -25,7 +25,7 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$http
 				$scope.authentication.user = response;
 
 				// And redirect to the index page
-				$location.path('/');
+				$location.path('/dashboard');
 			}).error(function(response) {
 				$scope.error = response.message;
 			});
