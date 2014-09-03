08aed7a4a7f0d7b8123bc3154264a9acd56cbbad
Carmen V. Krol <carmen.krol@gmail.com>
2014-09-03 12:42:23 -0400

    reset buttons for all timers

--- public/modules/core/controllers/timer.client.controller.js
+++ public/modules/core/controllers/timer.client.controller.js
@@ -3,13 +3,17 @@
 
 angular
   .module('core')
-  .controller('TimerController', ['$scope', '$timeout',
+  .controller('TimerController', ['$scope', '$timeout', '$rootScope',
 
-    function($scope, $timeout) {
+    function($scope, $timeout, $rootScope) {
 
       $scope.timerRunning = false;
 
       $scope.countdownPomodoro = 937500;
+
+      $scope.countdownShortBreak = 7500;
+
+      $scope.countdownLongBreak = 112500;
 
       $scope.startTimer = function (){
         $scope.$broadcast('timer-start');
@@ -21,10 +25,22 @@ angular
         $scope.timerRunning = false;
       };
 
-      $scope.resetTimer = function (){
-        $scope.startTimer();
-        $scope.countdownPomodoro = 937500;
-        $scope.$broadcast('timer-set-countdown', $scope.countdownPomodoro);
+      $scope.resetTimer = function (timerType){
+        if (timerType === 'pomodoro') {
+          $scope.startTimer();
+          $scope.countdownPomodoro = 937500;
+          $scope.$broadcast('timer-set-countdown', $scope.countdownPomodoro);
+        }
+        else if (timerType ==='shortBreak') {
+          $scope.startTimer();
+          $scope.countdownShortBreak = 7500;
+          $scope.$broadcast('timer-set-countdown', $scope.countdownShortBreak);
+        }
+        else if (timerType === 'longBreak') {
+          $scope.startTimer();
+          $scope.countdownLongBreak = 112500;
+          $scope.$broadcast('timer-set-countdown', $scope.countdownLongBreak);
+        }
       };
 
     }
